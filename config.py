54b285ea2da149a5ab3c4bf5cd1c19ac88a8de6b
CutyCat2000 <132785498+CutyCat2000@users.noreply.github.com>
2023-11-22 00:12:47 +0100

    V1.1

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import discord
 import os
 
-TOKEN = ""
+TOKEN = "" # Bot token
 
 embed_color = discord.Color.dark_embed()
 embed_color_error = discord.Color.dark_red()
@@ -9,4 +9,9 @@ embed_color_warning = discord.Color.orange()
 embed_color_success = discord.Color.dark_green()
 embed_color_premium = 0xFFC300
 
-channel_for_tickets = 
+channel_for_tickets = # Channel ID
+
+bot_name = '' # Bot name
+bot_avatar = '' # Link to avatar of bot
+bot_id = # Bot ID
+bot_secret = '' # Bot secret
