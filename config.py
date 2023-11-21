6d37dc8af6ecd3c295d7a1375ff21efd6816bb5b
CutyCat2000 <132785498+CutyCat2000@users.noreply.github.com>
2023-11-21 12:36:22 +0100

    V1.0-Channel

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ embed_color_error = discord.Color.dark_red()
 embed_color_warning = discord.Color.orange()
 embed_color_success = discord.Color.dark_green()
 embed_color_premium = 0xFFC300
+
+channel_for_tickets = 
